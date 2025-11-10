33469cde24bdd9dfa825cb1ea561482abc805f26
AltSystem42 <50642697+AltSystem42@users.noreply.github.com>
2025-11-10 18:42:59 -0400

    added longer number functionality
    added longer integers to be used

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const display = document.querySelector(".screen")
 
 window.addEventListener('keydown', (e) => {
-    const key = document.querySelector(`div[data-key="${e.keyCode}"`)
+    const key = document.querySelector(`div[data-key="${e.key}"`)
+    console.log(e.key)
     if(!key) return; // stop function from running on bad key.
     if(["1","2","3","4","5","6","7","8","9","0", "(", ")", "÷", "x", "-", ".", "+"].includes(key.textContent)){
         display.textContent += key.textContent
@@ -9,8 +10,16 @@ window.addEventListener('keydown', (e) => {
         display.textContent = ""
     } else if (key.textContent === "="){
         let string = display.textContent;
-        let total = operate(string[0], string[1], string[2])
+        const operators = ["x", "+", "÷", "-"]
+        const index = [...string].findIndex(ch => operators.includes(ch))
+        
+        if (index !== -1){
+        const left = string.slice(0, index);
+        const operator = string[index];
+        const right = string.slice(index + 1);
+        let total = operate(left, operator, right)
         display.textContent = total;
+        }
     }
     
 })
